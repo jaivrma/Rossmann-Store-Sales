52e77dc8b8a0b670030fa2b61cd466e3269c3e3b
Jai Verma <jaivrma956@gmail.com>
2024-07-29 23:47:32 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the data from GitHub raw URL
-url = 'https://raw.githubusercontent.com/jaivrma/Rossmann-Store-Sales/main/projectdata.csv'
+url = 'https://github.com/jaivrma/Rossmann-Store-Sales/blob/main/projectdata.csv'
 dataframe = pd.read_csv(url, low_memory=False)
 
 # Print column names to inspect them
